dad6890d916f54a2580456704d93b78c874c855b
William Gaertner <william_nefi@hotmail.com>
2019-08-27 20:54:17 -0400

    Projeto conta

---
+++ Conta/ContaUsuario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Conta
+{
+    class ContaUsuario
+    {
+        public int Conta { get; private set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaUsuario(int conta, string nome)
+        {
+            Conta = conta;
+            Nome = nome;
+        }
+
+        public ContaUsuario(int conta, string nome, double saldo) : this(conta, nome)
+        {
+            Saldo = saldo;
+        }
+    
+        public void Deposito(double deposito)
+        {
+            Saldo += deposito;
+        }
+
+        public void Saque(double saque)
+        {
+            Saldo -= saque + 5.00;
+        }
+        public override string ToString()
+        {
+            return "Conta: " + Conta + ", Titular: " + Nome + ", Saldo: $ " + Saldo;
+        }
+    }
+}
---
+++ Conta/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Conta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaUsuario Contas;
+
+            Console.Write("Entre o numero da conta: ");
+            int Numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre nome usuario: ");
+            string Nome = Console.ReadLine();
+            Console.Write("Havera deposito (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+            if(resp == 's' || resp == 'S')
+            {
+                Console.Write("Qual o valor para deposito: ");
+                double DepInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Contas = new ContaUsuario(Numero, Nome, DepInicial);
+            }
+            else
+            {
+                Contas = new ContaUsuario(Numero, Nome);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados usuario");
+            Console.WriteLine(Contas);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para deposito: ");
+            double DepositoIncremento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Contas.Deposito(DepositoIncremento);
+            Console.WriteLine("dados atualizados");
+            Console.WriteLine(Contas);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            double SaqueIncremento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Contas.Saque(SaqueIncremento);
+            Console.WriteLine("dados atualizados");
+            Console.WriteLine(Contas);
+            Console.ReadLine();
+        }
+    }
+}
